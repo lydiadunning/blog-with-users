318d0be1ccdcda1665c9ead44a9cadbd5ff01472
Lydia Dunning <lydia.dunning@gmail.com>
2022-03-29 22:56:47 -0700

    Include database instantiation. Delete existing database.

--- main.py
+++ main.py
@@ -37,16 +37,6 @@ class User(db.Model, UserMixin):
     blog_posts = relationship("BlogPost", back_populates="author")
     comments = relationship("BlogComment", back_populates="author")
 
-# class Parent(db.Model):
-#     __tablename__ = 'parent'
-#     id = db.Column(db.Integer, primary_key=True)
-#     children = relationship("Child", back_populates="parent")
-#
-# class Child(db.Model):
-#     __tablename__ = 'child'
-#     id = db.Column(db.Integer, primary_key=True)
-#     parent_id = db.Column(db.Integer, db.ForeignKey('parent.id'))
-#     parent = relationship("Parent", back_populates="children")
 
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
