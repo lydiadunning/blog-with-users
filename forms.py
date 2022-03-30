756f0b0b7ba86a2086c4a6b593d70c6e9dda5c55
Lydia Dunning <lydia.dunning@gmail.com>
2022-03-29 21:38:26 -0700

    attempt to resolve application error by updating requirements.txt and modifying app.run

--- forms.py
+++ forms.py
@@ -14,7 +14,7 @@ class CreatePostForm(FlaskForm):
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     name = StringField("Name", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Register")
 
 class LoginForm(FlaskForm):
